8d13d0522d4ce27c780b773bf4120f76b6ad0843
Chris <46077768+t-chris@users.noreply.github.com>
2019-04-30 11:05:53 -0700

    conversion of textarea string by .split and parsing for doubles

--- Statistics.java
+++ Statistics.java
@@ -1,11 +1,21 @@
 import java.util.Arrays;
 
 public class Statistics {
+    public static String sortedString(double[] d) {
+        String sorted = "";
+        for (int i = 0 ; i < d.length ; i++) {
+            sorted = sorted + d[i] + " ";
+        }
+        return sorted;
+    }
+   
+    
+    
     public static double[] convertData(String s) { //Converts textbox string into doubles array.
         String[] strData = s.split(" ");
         double[] data = new double[strData.length];
         for (int i = 0 ; i < strData.length ; i++) {
-            data[i] = Integer.parseInt(strData[i]);
+            data[i] = Double.parseDouble(strData[i]);
         }
         return data; //Data will be used to calculate everything else.
     }
@@ -21,10 +31,10 @@ public class Statistics {
     public static double median(double[] d) {
         Arrays.sort(d);
         if (d.length%2 == 0) {
-            return (d[d.length%2] + d[d.length%2-1])/2;
+            return (d[d.length/2] + d[d.length/2-1])/2.0;
         }
         else {
-            return d[(int)d.length%2];
+            return d[d.length/2];
         }
     }
     
@@ -54,14 +64,14 @@ public class Statistics {
     public static double var(double[] d) {
         double tempVal = 0;
         for (int i = 0 ; i < d.length ; i++) {
-            tempVal += (d[i] - mean(d))*(d[i] - mean(d));
+            tempVal += (mean(d) - d[i])*(mean(d) - d[i]);
         }
         
         return tempVal/(d.length-1);
     }
     
     public static double std (double[] d) {
-        return var(d)*var(d);
+        return Math.sqrt(var(d));
     }
     
     public static double max (double[] d) {
